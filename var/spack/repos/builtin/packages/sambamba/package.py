c3b685b24a11e758995b190f15ec80666ed1a6ee
Emilio Palumbo <emiliopalumbo@gmail.com>
2023-06-30 12:18:07 +0200

    Update sambamba package

--- var/spack/repos/builtin/packages/sambamba/package.py
+++ var/spack/repos/builtin/packages/sambamba/package.py
@@ -6,24 +6,26 @@
 from spack.package import *
 
 
-class Sambamba(Package):
+class Sambamba(MakefilePackage):
     """Sambamba: process your BAM data faster (bioinformatics)"""
 
     homepage = "https://lomereiter.github.io/sambamba/"
     git = "https://github.com/lomereiter/sambamba.git"
 
+    version("1.0.0", tag="v1.0.0", submodules=True)
+    version("0.7.1", tag="v0.7.1", submodules=True)
     version("0.6.6", tag="v0.6.6", submodules=True)
 
     depends_on("ldc~shared", type=("build", "link"))
     depends_on("python", type="build")
+    depends_on("lz4", type=("build","link"))
 
     resource(name="undeaD", git="https://github.com/dlang/undeaD.git", tag="v1.0.7")
 
-    patch("Makefile.patch")
-    parallel = False
+    patch("Makefile.patch", when="@0.6.6")
+
+    build_targets = ['static']
 
     def install(self, spec, prefix):
-        make("sambamba-ldmd2-64")
         mkdirp(prefix.bin)
-        for filename in ("build/sambamba", "build/sambamba.debug"):
-            install(filename, prefix.bin)
+        install('bin/sambamba-{}'.format(spec.version), '{}/sambamba'.format(prefix.bin))
